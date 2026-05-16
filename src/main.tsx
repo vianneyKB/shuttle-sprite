736852d5c64dd90a40b10c169578b85a024c3a45
v1ann3yKB <vianneykondo@yahoo.fr>
2026-05-16 19:26:40 +0200

    adding browser router and base name

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "leaflet/dist/leaflet.css";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+    <BrowserRouter basename="/shuttle-sprite">
+      <App />
+    </BrowserRouter>);
