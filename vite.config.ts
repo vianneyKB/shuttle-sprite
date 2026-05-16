2a60c59be26b4a683eea6a092b185ee8915dcdb1
v1ann3yKB <vianneykondo@yahoo.fr>
2026-05-16 18:33:02 +0200

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+// import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -15,4 +16,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/shuttle-sprite/'
 }));
