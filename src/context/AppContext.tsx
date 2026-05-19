4127a532b7d6b7173b680fe9a5b237becf6fccf1
v1ann3yKB <vianneykondo@yahoo.fr>
2026-05-19 09:43:17 +0200

    Remove env files and fixing main root to DOM

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -57,6 +57,7 @@ export const AppProvider: React.FC<{ children: ReactNode }> = ({ children }) => 
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAppContext = () => {
   const context = useContext(AppContext);
   if (context === undefined) {
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "leaflet/dist/leaflet.css";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+// import { createRoot } from "react-dom/client";
+// import App from "./App.tsx";
+// import "leaflet/dist/leaflet.css";
+// import "./index.css";
+// import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(
+// createRoot(document.getElementById("root")!).render(
+//     <BrowserRouter basename="/shuttle-sprite">
+//       <App />
+//     </BrowserRouter>);
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { AppProvider } from "@/context/AppContext";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <BrowserRouter basename="/shuttle-sprite">
-      <App />
-    </BrowserRouter>);
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+const redirect = sessionStorage.redirect;
+
+delete sessionStorage.redirect;
+
+if (redirect && redirect !== location.href) {
+  history.replaceState(null, "", redirect);
+}
